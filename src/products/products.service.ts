3662961d128c3867766addc039cb63e9411d0204
Stefano Garbocci <stefgarbocci@gmail.com>
2023-07-13 20:06:43 -0300

    feat(products.service): Agregado metodo create

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, Logger, BadRequestException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+
+  private readonly logger = new Logger('ProductsService');
+  
+  constructor(
+
+    @InjectRepository(Product)
+    private readonly productsRepository: Repository<Product>,
+
+  ) {}
+
+
+  async create(createProductDto: CreateProductDto) {
+    
+    try {
+      const product = this.productsRepository.create(createProductDto);
+      await this.productsRepository.save(product);
+
+      return product;
+      
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
   }
 
   findAll() {
@@ -18,9 +42,21 @@ export class ProductsService {
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
-  }
+  } 
 
   remove(id: number) {
     return `This action removes a #${id} product`;
   }
+
+
+  private handleDBExceptions(error: any) {
+
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+
+    this.logger.error(error);
+    throw new InternalServerErrorException('Unexpected error, check server logs');
+  }
+
 }
